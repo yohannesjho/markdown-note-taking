6b9b05a009e46bf8a71fa8fe2d8546c658578424
yohannesjho <jhonians@outlook.com>
2024-11-17 18:10:02 +0300

    remove local domain

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ dotenv.config()
 const app = express()
 const corsOptions = {
     origin: [
-      'http://localhost:3000',
+      
       'https://markdown-front-end.vercel.app/'  
       
     ],
